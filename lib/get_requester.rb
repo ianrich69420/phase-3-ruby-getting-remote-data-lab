9f964c40c9fdb74a989b97796ef011873444f5a7
ianrich69420 <ian.orieko@student.moringaschool.com>
2023-03-06 00:35:42 +0300

    Completed assignment

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,18 @@
+require 'open-uri'
+require 'json'
+
+
 # Write your code here
+class GetRequester
+    def initialize(url)
+        @url = url unless url.class != String
+    end
+
+    def get_response_body
+        URI.parse(@url).open.string
+    end
+    
+    def parse_json
+        JSON.parse(get_response_body)
+    end
+end
